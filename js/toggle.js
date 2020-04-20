e89ff42b33559fb2c8f69a7ba75222f6f7457361
chidiu98 <34973870+koji98@users.noreply.github.com>
2020-04-20 19:26:55 -0400

    First Blog Post

--- js/toggle.js
+++ js/toggle.js
@@ -1,15 +1,46 @@
-$("#customSwitch1").on("click", () => {
+const toggle = $("#customSwitch1");
+
+if (localStorage.getItem("theme").includes("dark.css")) {
+    toggle.prop("checked", true);
+    setStyleSheet("dark")
+    $("#customSwitch1Label").html("&larr;");
+} else {
+    toggle.prop("checked", false);
+    setStyleSheet("light")
+    $("#customSwitch1Label").html("&rarr;");
+}
+
+toggle.on("click", () => {
     swapStyleSheet()
 })
 
-function swapStyleSheet(){
+function swapStyleSheet() {
     let cssLink = document.getElementById("cssTheme");
     let currHref = cssLink.getAttribute("href");
-    if (currHref === "./light.css") {
-        cssLink.setAttribute("href", "./dark.css")
-        $("#customSwitch1Label").html("&larr;")
+    let currUrl = window.location.href;
+    let directory = currUrl.includes("/blog_posts/") ? ".." : ".";
+
+    if (currHref === `${directory}/light.css`) {
+        let theme = `${directory}/dark.css`;
+        cssLink.setAttribute("href", theme);
+        $("#customSwitch1Label").html("&larr;");
+        localStorage.setItem("theme", theme);
+        localStorage.setItem("arrow", "&larr;");
     } else {
-        cssLink.setAttribute("href", "./light.css")
-        $("#customSwitch1Label").html("&rarr;")
+        let theme = `${directory}/light.css`;
+        cssLink.setAttribute("href", theme);
+        $("#customSwitch1Label").html("&rarr;");
+        localStorage.setItem("theme", theme);
+        localStorage.setItem("arrow", "&rarr;")
     }
+}
+
+function setStyleSheet(sheet) {
+    let cssLink = document.getElementById("cssTheme");
+    let currHref = cssLink.getAttribute("href");
+    let currUrl = window.location.href;
+    let directory = currUrl.includes("/blog_posts/") ? ".." : ".";
+    let theme = `${directory}/${sheet}.css`;
+
+    cssLink.setAttribute("href", theme);
 }
